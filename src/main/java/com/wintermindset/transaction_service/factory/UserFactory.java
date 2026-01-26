c3e832494e9949e54067390a8dfd313456356aa6
wintermindset <wintermindset@yandex.ru>
2026-01-25 23:46:16 -0800

    Factory update due to User Entity soft deletion realisation
    Now createUser also requires user's creation time and validates this in Default Factory.
    
    Closes #9

--- src/main/java/com/wintermindset/transaction_service/factory/UserFactory.java
+++ src/main/java/com/wintermindset/transaction_service/factory/UserFactory.java
@@ -1,8 +1,15 @@
 package com.wintermindset.transaction_service.factory;
 
+import java.time.Instant;
+
 import com.wintermindset.transaction_service.entity.Role;
 import com.wintermindset.transaction_service.entity.UserEntity;
 
 public interface UserFactory {
-    public UserEntity createUser(String username, String passwordHash, Role role);
+    public UserEntity createUser(
+            String username,
+            String passwordHash,
+            Role role,
+            Instant creationTime
+    );
 }
