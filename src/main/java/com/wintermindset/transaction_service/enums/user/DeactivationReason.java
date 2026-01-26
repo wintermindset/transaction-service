161dd414cae2ec046aee92767bc93ea8d7dcf287
wintermindset <wintermindset@yandex.ru>
2026-01-26 02:10:18 -0800

    Project structure and code refactor
    - User enums were moved to enums/user.
    - Some unnecessary blank lines of code were removed.
    - Some basic syntax formatting was done.
    
    Refs #12

---
+++ src/main/java/com/wintermindset/transaction_service/enums/user/DeactivationReason.java
@@ -0,0 +1,11 @@
+package com.wintermindset.transaction_service.enums.user;
+
+public enum DeactivationReason {
+    USER_REQUEST,
+    ADMIN_ACTION,
+    SECURITY_BREACH,
+    INACTIVITY,
+    TERMS_VIOLATION,
+    FRAUD_DETECTION,
+    SYSTEM_ERROR
+}
